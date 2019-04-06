058b630ad8d19365fe6e3f3f27a2ff493da3aed2
nice <17634053215@163.com>
2019-04-06 22:15:01 +0800

    drag over

--- demo.js
+++ demo.js
@@ -1,9 +1,19 @@
 function wo(arg){
-	console.log(arg)
-	console.log("aaa")
-	var distx=0  //撤销提交状态
-	var disty=0  //撤销提交状态
-
-	alert("我是new1分支上修改的内容")
+	arg.onmousedown=function(e){
+		var ev=e||window.event
+		var wid=ev.clientX-arg.offsetLeft
+		var hei=ev.clientY-arg.offsetTop
+		console.log(wid)
+			document.onmousemove=function(e){
+				var ec=e||window.event
+				console.log(ec.clientX,ec.clientY)
+				arg.style.top=ec.clientY-hei+"px"
+				arg.style.left=ec.clientX-wid+"px"
+			}
+	}
+	arg.onmouseup=function(e){
+		console.log("aa")
+			document.onmousemove=null
+		}
 
 }
